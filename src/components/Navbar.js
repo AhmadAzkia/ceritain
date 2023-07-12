355dfba14b513e47a1be4ca8fa7d1e0499222a07
AhmadAzkia <ahmad.10122065@mahasiswa.unikom.ac.id>
2023-07-12 20:06:49 +0700

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,15 +8,19 @@ function Header() {
   return (
 <div>
     {/* Header */}
-    <nav className="bg-black w-full shadow-transparent2 flex-col">
+    <nav className="bg-white w-full shadow-transparent2 flex-col">
       <div className="flex flex-col md:flex-row">
         <div className={`flex justify-between items-center py-6 px-6 md:border-none ${isOpen ? 'border-b' : 'none'}`}>
             <div>
-              {/* Disini isi dengan Kata Ceritain */}
+              <h1 className="text-2xl md:text-3xl fontLogo ml-3">
+                {/* Span untuk membuat huruf C jadi lebih besar */}
+                <span className="text-5xl">C</span> 
+                ERITAIN
+              </h1>
             </div>
             <div className="items-center block md:hidden">
               <button onClick={() => setIsOpen(!isOpen)}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-7 h-7">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-7 h-7">
                   <path className={`${!isOpen ? 'block' : 'hidden'}`} strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                   <path className={`${isOpen ? 'block' : 'hidden'}`} strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
@@ -26,12 +30,12 @@ function Header() {
 
         <div className={`${isOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row justify-between w-full md:items-center`}>
           <div className="flex flex-col md:flex-row mx-auto">
-            <a href="#" className="text-white block px-6 py-2 fontHeader md:text-2xl md:px-16">About</a>
-            <a href="#" className="text-white block px-6 py-2 fontHeader md:text-2xl md:px-16" >Roadmap</a>
-            <a href="#" className="text-white block px-6 py-2 fontHeader md:text-2xl md:px-16">Wen</a>
+            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-2xl md:px-16">Janji Tatap Muka</a>
+            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-2xl md:px-16" >Testimoni</a>
+            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-2xl md:px-16">Tentang Kami</a>
           </div>
           <div className="flex flex-col md:flex-row px-4 py-4">
-            <button className="bg-white rounded-full w-36 md:h-8 md:w-36 fontHeader text-sm">Connect Wallet</button>
+            <button className="bgWarna rounded-full w-36 md:h-10 md:w-36 fontNavbar text-xl">Login</button>
           </div>
         </div>
       </div>  
