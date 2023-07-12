c4cfc07d58f332b3a9c3a7d76615a9ad229721e3
AhmadAzkia <ahmad.10122065@mahasiswa.unikom.ac.id>
2023-07-12 20:17:57 +0700

    perkecic semua font navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,9 @@ function Header() {
       <div className="flex flex-col md:flex-row">
         <div className={`flex justify-between items-center py-6 px-6 md:border-none ${isOpen ? 'border-b' : 'none'}`}>
             <div>
-              <h1 className="text-2xl md:text-3xl fontLogo ml-3">
+              <h1 className="text-2xl md:text-2xl fontLogo ml-3">
                 {/* Span untuk membuat huruf C jadi lebih besar */}
-                <span className="text-5xl">C</span> 
+                <span className="text-4xl">C</span> 
                 ERITAIN
               </h1>
             </div>
@@ -30,12 +30,12 @@ function Header() {
 
         <div className={`${isOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row justify-between w-full md:items-center`}>
           <div className="flex flex-col md:flex-row mx-auto">
-            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-2xl md:px-16">Janji Tatap Muka</a>
-            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-2xl md:px-16" >Testimoni</a>
-            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-2xl md:px-16">Tentang Kami</a>
+            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-1xl md:px-16">Janji Tatap Muka</a>
+            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-1xl md:px-16" >Testimoni</a>
+            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-1xl md:px-16">Tentang Kami</a>
           </div>
           <div className="flex flex-col md:flex-row px-4 py-4">
-            <button className="bgWarna rounded-full w-36 md:h-10 md:w-36 fontNavbar text-xl">Login</button>
+            <button className="bgWarna rounded-full w-36 md:h-9 md:w-36 fontNavbar text-lg">Login</button>
           </div>
         </div>
       </div>  
