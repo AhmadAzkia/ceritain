6b41f402e0faec7976dc16b36fb0edcfe8e4a4ea
AhmadAzkia <ahmad.10122065@mahasiswa.unikom.ac.id>
2023-07-14 01:40:08 +0700

    create jumbotron, tolong di cek hasilnya. minta saran karna udah mentok bgt

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,7 +3,22 @@ import React from "react";
 function Jumbotron() {
   return (
     <div>
-        Hallo
+      <div className="md:p-8">
+        <h1 className="px-4 mb-2 text-sm font-extrabold leading-none md:text-3xl">
+          Cek Kesehatan Mentalmu Bersama Dokter Andalan Kami
+        </h1>
+        <p className="px-4 mb-4 md:mb-0 sm:px-4 text-xs md:text-base">
+          Kamu penasaran dengan kondisi kesehatan mentalmu saat ini? Yuk, segera buat janji dengan dokter andalan kami untuk mengetahui hasilnya!
+        </p>
+      </div>
+      <div>
+        <a href="#" class="inline-flex justify-center items-center mx-5 py-3 px-4 text-xs text-center text-white rounded-lg bg-[#5A96E3] hover:bg-blue-800 md:mx-20 md:text-sm">
+          Booking Sekarang
+          <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
+import '../App.css';
 import '../css/font.css';
-import { useNavigate } from "react-router-dom";
 
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false)
-  let navigate = useNavigate()
   return (
 <div>
     {/* Header */}
@@ -15,7 +14,7 @@ function Header() {
             <div>
               <h1 className="text-2xl md:text-2xl fontLogo ml-4">
                 {/* Span untuk membuat huruf C jadi lebih besar */}
-                <span className="text-4xl">C</span> 
+                <span className="text-4xl">C</span>
                 ERITAIN
               </h1>
             </div>
@@ -29,14 +28,14 @@ function Header() {
             </div>
         </div>
 
-        <div className={`${isOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row justify-between w-full md:items-center`}>
+        <div className={`${isOpen ? 'block' : 'hidden'} md:flex flex-col md:flex-row justify-between w-full md:items-center border md:border-none mb-2 md:mb-0`}>
           <div className="flex flex-col md:flex-row mx-auto">
             <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-1xl md:px-16">Janji Tatap Muka</a>
-            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-1xl md:px-16" >Testimoni</a>
+            <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-1xl md:px-16">Testimoni</a>
             <a href="#" className="text-black block px-6 py-2 fontNavbar md:text-1xl md:px-16">Tentang Kami</a>
           </div>
           <div className="flex flex-col md:flex-row px-4 py-4">
-            <button className="bgWarna rounded-2xl w-36 md:h-9 md:w-36 fontNavbar text-lg hover:shadow-md mr-4" onClick={()=>navigate('/login')}>Login</button>
+            <button className="bgWarna rounded-2xl w-36 md:h-9 md:w-36 fontNavbar text-lg hover:shadow-md mr-4">Login</button>
           </div>
         </div>
       </div>  
