36a819a4f8f02f90f143bb7f7daae5838d94ad4f
darwannneee <darwanchannel1@gmail.com>
2023-07-15 22:07:13 +0700

    Restore Files

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('ceritain', 'Ceritain', 'LombaDb@3', {
-    host: 'ceritain.mysql.database.azure.com',
-    dialect: "mysql"
-})
-
-export default db;
--- backend/routes/User_Route.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import {getUser} from "../controllers/User_Controller.js"
-
-const router = express.Router();
-router.get('/users', getUser);
-
-export default router;
