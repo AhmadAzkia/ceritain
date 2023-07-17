10d4f41a9f9f6ef4e27b016394a96ccb43a48a3d
AhmadAzkia <ahmad.10122065@mahasiswa.unikom.ac.id>
2023-07-17 07:13:49 +0700

    update jumbotron

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Main from './components/Jumbotron';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import { render } from "react-dom";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
@@ -14,7 +10,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <FontAwesomeIcon icon={faHome} />
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { render } from 'react-dom';
 
-// import your fontawesome library
-import './fontawesome';
 
 render(<App />, document.getElementById('root'));
 
