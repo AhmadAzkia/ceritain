5fe0fab85e7641300d50bdf33ae55f6ff005e1a9
DifaaNug <123245678+DifaaNug@users.noreply.github.com>
2023-07-19 03:32:24 +0700

    Update Register.js

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -13,17 +13,19 @@ function Register() {
     <Navbar />
     <form>
       <div className="w-full min-h-screen grid place-items-center bg-[#F6F8FD]">
-        <div className="w-96 bg-white shadow rounded-xl px-14 -mt-20">
+        <div className="w-96 bg-white shadow rounded-xl px-14">
           <h1 className="text-center text-2xl fontLogo mt-8 mb-2">
                   <span className="text-4xl">C</span>
                   ERITAIN
                 </h1>
           <p className="text-center text-slate-700 fontLoginn text-sm">Create your account now</p>
           <div className="flex flex-col mt-7 gap-2 fontLoginn">
+            <p className="text-sm fontLoginn">Name*</p>
+            <input className="w-full h-10 ps-2 border rounded-md text-sm" type="text" name="name" id="name" placeholder="Name" required  />
             <p className="text-sm fontLoginn">Username*</p>
             <input className="w-full h-10 ps-2 border rounded-md text-sm" type="text" name="username" id="username" placeholder="Username" required  />
             <p className="text-sm fontLoginn">Password*</p>
-            <input className="w-full h-10 ps-2 border rounded-md text-sm" type="text" name="username" id="username" placeholder="Password" required  />
+            <input className="w-full h-10 ps-2 border rounded-md text-sm" type="password" name="Password" id="Password" placeholder="Password" required  />
           </div>
           <button className="w-full h-10 bg-blue-300 font-semibold rounded-md text-sm fontLoginn bg-opacity-75 mt-10" type="submit">Sign Up</button>
           <div className="text-xs fontLoginn mt-4 text-center">-- Or --</div>
