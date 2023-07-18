27712102fa0a0dabadd2bc41d6d5bc0ca836d32a
darwannneee <darwanchannel1@gmail.com>
2023-07-18 21:22:29 +0700

    Menambahkan Register Page

---
+++ src/Pages/Register.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
+import React, { useState } from "react";
+import '../css/font.css';
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/home/Navbar";
+import axios from "axios";
+
+function Register() {
+
+  return (
+    <>
+    <Navbar />
+   </>
+  );
+}
+
+export default Login;
