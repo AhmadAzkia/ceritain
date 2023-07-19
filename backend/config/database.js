feca3b6c80ea5ebe1ad448b333fa459df58a6e7c
darwannneee <darwanchannel1@gmail.com>
2023-07-20 06:35:50 +0700

    Fix config backend nya menambahkan mysql2

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,10 @@ const db = new Sequelize('ceritain', 'Ceritain', 'LombaDb@3', {
     host: 'ceritain.mysql.database.azure.com',
     dialect: "mysql", 
     define: {
-        timestamps: false
+        timestamps: false,
+        dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
     }
 })
 
