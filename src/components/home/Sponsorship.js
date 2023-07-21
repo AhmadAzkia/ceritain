6ec8a3be8f80a185a056cd30e6ff43504500cfc5
darwannneee <darwanchannel1@gmail.com>
2023-07-21 20:00:44 +0700

    Menambahkan Halaman Sponsorship dan AboutUs

---
+++ src/components/home/Sponsorship.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import '../../App.css';
+import '../../css/font.css';
+import { useNavigate } from "react-router-dom";
+
+function Sponsorship() {
+  return (
+    <div>
+   
+    </div>
+  );
+}
+
+export default Sponsorship;
