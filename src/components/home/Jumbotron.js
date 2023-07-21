ea9d9620edd5e9fb40bd6a871af5c1a866ee4ba6
AhmadAzkia <ahmad.10122065@mahasiswa.unikom.ac.id>
2023-07-21 17:24:34 +0700

    darwan jelek

--- src/components/home/Jumbotron.js
+++ src/components/home/Jumbotron.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../img/jumbotron.png";
 import '../../css/font.css';
+import '../../App.css';
 
 
 function Jumbotron() {
@@ -25,7 +26,7 @@ function Jumbotron() {
           </div>
 
             <div>
-              <p className="px-4 ml-3 mt-2 md:mt-4 md:ml-3 md:mr-5 md:mb-0 sm:px-4 text-xs md:text-sm md:text-base fontJumbotron2">
+              <p className="px-4 ml-3 mt-2 md:mt-4 md:ml-3 md:mr-5 md:mb-0 sm:px-4 text-xs md:text-sm fontJumbotronn">
                 Ceritain dibuat sebagai wadah untuk berkumpulkan orang yang membutuhkan seseorang untuk mendengarkan ceritanya. Pendengar yang berada di Ceritain semuanya adalah ahli di bidang psikologi!
               </p>
             </div>
