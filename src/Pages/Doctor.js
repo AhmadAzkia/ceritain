7c596eac9ebc25ff3a7bd99776b6af838c749fbf
darwannneee <darwanchannel1@gmail.com>
2023-07-23 14:35:21 +0700

    Menambahkan Halaman Pages

---
+++ src/Pages/Doctor.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import '../../App.css';
+import '../../css/font.css';
+import { useNavigate } from "react-router-dom";
+
+function Doctor() {
+  return (
+    <div>
+   
+    </div>
+  );
+}
+
+export default Doctor;
