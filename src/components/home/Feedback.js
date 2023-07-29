336e82d705e149dd39c0c6e28d860aa23ec7ab11
darwannneee <darwanchannel1@gmail.com>
2023-07-29 12:27:59 +0700

    Membuat File Feedback.js

---
+++ src/components/home/Feedback.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../../App.css';
+import '../../css/font.css';
+
+function Feedback() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Feedback;
