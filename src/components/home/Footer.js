a73c63a8d6d2c79afd8a258963e3c64df9f22aa9
AhmadAzkia <ahmad.10122065@mahasiswa.unikom.ac.id>
2023-08-03 21:31:13 +0700

    ubah ukuran font footer

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -5,9 +5,9 @@ import '../../css/font.css';
 function Footer() {
   return (
    <div className="border rounded-lg text-center h-24 flex items-center justify-center shadow-inner">
-      <a href="/" className="text-2xl fontLogo">
+      <a href="/" className="text-2xl md:text-3xl fontLogo">
         {/* Span untuk membuat huruf C jadi lebih besar */}
-        <span className="text-4xl">C</span>
+        <span className="text-4xl md:text-5xl">C</span>
         ERITAIN
       </a>
    </div>
