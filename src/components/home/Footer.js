0f63c8c91e7e53774fb0e40a73cf87241a0becf5
darwannneee <darwanchannel1@gmail.com>
2023-08-04 14:57:47 +0700

    Merubah footer

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -4,7 +4,7 @@ import '../../css/font.css';
 
 function Footer() {
   return (
-   <div className="border rounded-lg text-center h-24 flex items-center justify-center shadow-inner">
+   <div className="rounded-lg text-center mt-14 h-24 flex items-center justify-center shadow-inner md:mt-20">
       <a href="/" className="text-2xl md:text-3xl fontLogo">
         {/* Span untuk membuat huruf C jadi lebih besar */}
         <span className="text-4xl md:text-5xl">C</span>
