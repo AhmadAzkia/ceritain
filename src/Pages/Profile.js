25996e973fb15db5f353eff8fc7bad74aa9e71a3
darwannneee <darwanchannel1@gmail.com>
2023-08-05 09:18:08 +0700

    Menambahkan Session di halaman Profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Navbar from "../components/home/Navbar";
+import Footer from "../components/home/Footer";
+import { useNavigate } from "react-router-dom";
 import '../App.css'
 import '../css/font.css';
 
 function Profile() {
+  let navigate = useNavigate()
+
+    // Cek apakah data sesi (misalnya token) ada dalam localStorage
+    const isLoggedIn = !!localStorage.getItem('token');
+
+    if (!isLoggedIn) {
+      // Jika pengguna sudah login, arahkan ke halaman lain (misalnya halaman profile)
+      navigate('/login');
+      return null;
+    }
   return (
    <>
    <Navbar />
+
+   <Footer />
    </>
   );
 }
